c4bf39f45c5beb8980ebf86f9598cc6ccf7867a3
Nerdy-Thanos <47348592+Nerdy-Thanos@users.noreply.github.com>
2019-06-18 15:13:30 +0530

    waypoint
    this is the location script. give the current location of the enemy and helps to  find a path across the map

---
+++ Waypoint.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+
+
+    const int gridSize = 10;
+    Vector2Int gridPos;
+
+    public bool isExplored = false;
+
+    public bool isPlacable = true;
+
+    // Use this for initialization
+
+    public Waypoint exploredFrom;
+
+
+    public Vector2Int GetGridPos()
+    {
+        return new Vector2Int(
+                Mathf.RoundToInt(transform.position.x / gridSize),
+                Mathf.RoundToInt(transform.position.z / gridSize)
+                          );
+    }
+    public int Getgridsize()
+    {
+        return gridSize;
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isPlacable)
+            {
+                FindObjectOfType<TowerFactory>().AddTower(this);
+            }
+            else
+            {
+                print("cant place here");
+            }
+        }
+    }
+
+}
